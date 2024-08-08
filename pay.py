ccf4a72ce37ccd6b4082aafa98df06a85bf771c3
setayeshsaemi <setayeshsaemi3@gmail.com>
2024-08-08 15:36:47 +0330

    تمرین2

---
+++ pay.py
@@ -0,0 +1,18 @@
+i = 0
+sum = 0
+avg =0
+x = int(input('enter your number'))
+while i<x:
+     c = float(input('enter your numbers'))
+     c = max
+     c = min
+    sum = sum+c
+    i=i+1
+    if c>max:
+        print('c=max')
+    else:
+        if c<min:
+            print('c=min')
+i =i+1
+avg = sum/x
+print('avg')
